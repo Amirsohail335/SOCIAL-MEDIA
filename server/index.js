a05c5f01b67b0e3b7bf7b0c0514771625eb5f9e1
Amir Sohail <amirsohail335@gmail.com>
2023-01-06 16:53:09 +0530

    committed

--- server/index.js
+++ server/index.js
@@ -16,12 +16,12 @@ app.use(morgan("common"));
 app.use("/auth", authRouter);
 app.use("/posts", postsRouter);
 app.get("/", (req, res) => {
-    res.status(200).send("OK from Server");
+  res.status(200).send("OK from Server");
 });
 
 const PORT = process.env.PORT || 4001;
 
 dbConnect();
 app.listen(PORT, () => {
-    console.log(`listening on port: ${PORT}`);
+  console.log(`listening on port: ${PORT}`);
 });
